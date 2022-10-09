08abdb23e49d31531d071f4362119c0b0284bdf9
Tércio Tales Barnabé <terciotalesb@gmail.com>
2022-10-08 22:41:48 -0300

    Finaliza inversor de palavras

--- app/src/main/java/com/example/trabalhoavaliativo/inversorPalavras.java
+++ app/src/main/java/com/example/trabalhoavaliativo/inversorPalavras.java
@@ -2,13 +2,38 @@ package com.example.trabalhoavaliativo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class inversorPalavras extends AppCompatActivity {
+
+    EditText palavra;
+    Button botao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inversor_palavras);
+
+        palavra = findViewById(R.id.ed01);
+        botao = findViewById(R.id.btn01);
+
+        botao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!palavra.getText().toString().isEmpty()) {
+                    Intent intent = new Intent(inversorPalavras.this, palavraInvertida.class);
+                    intent.putExtra("palavra", palavra.getText().toString());
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Toast.makeText(inversorPalavras.this, "Nenhum texto digitado!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/trabalhoavaliativo/palavraInvertida.java
+++ app/src/main/java/com/example/trabalhoavaliativo/palavraInvertida.java
@@ -2,13 +2,23 @@ package com.example.trabalhoavaliativo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class palavraInvertida extends AppCompatActivity {
+
+    TextView palavra_invertida;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_palavra_invertida);
+
+        palavra_invertida = findViewById(R.id.palavra_invertida);
+
+        Intent dadosRecebidos = getIntent();
+        String palavra = dadosRecebidos.getStringExtra("palavra");
+        palavra_invertida.setText(new StringBuilder(palavra).reverse().toString());
     }
 }
